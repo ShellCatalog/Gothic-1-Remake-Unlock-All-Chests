68173a7aa76453fe0e6f659b13ffb84482d0e141
ShellCatalog <barkerrhea9@gmail.com>
2026-06-07 17:42:59 +0100

    Create UnlockManager.cpp

---
+++ scr/UnlockManager.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+class UnlockManager
+{
+public:
+    void UnlockChest()
+    {
+        std::cout << "Chest unlocked." << std::endl;
+    }
+
+    void UnlockDoor()
+    {
+        std::cout << "Door unlocked." << std::endl;
+    }
+};
